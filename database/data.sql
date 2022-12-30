84ca9b8ce50a31ae0a9b21df8b6eed2772b282fd
Aaron Butnik <aaronjbutnik@gmail.com>
2022-12-30 21:47:47 +0000

    added dummy data to data.sql, imported schema from db desgin to schema.sql #1

---
+++ database/data.sql
@@ -0,0 +1,5 @@
+insert into "users" ("firstName", "lastName")
+values ('Bob', 'Ross');
+
+insert into "vehicle" ("userId","year", "make", "model", "licensePlate", "odometer", "notes")
+values ('1','2007', 'Subaru', 'WRX', '6htv695', '21456', 'needs new tires');
