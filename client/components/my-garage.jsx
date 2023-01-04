d57c97d7be4a534e23d4b1b77a8212bc83ad6adc
Aaron Butnik <aaronjbutnik@gmail.com>
2023-01-04 21:54:14 +0000

    Competed CSS styles for my-garage page.  Tested front end and back end connection for add vehicle route.  Tested error handling for add vehicle route. #1

--- client/components/my-garage.jsx
+++ client/components/my-garage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { FaArrowAltCircleDown } from 'react-icons/fa';
 
 export default class MyGarage extends React.Component {
   constructor(props) {
@@ -33,11 +34,48 @@ export default class MyGarage extends React.Component {
           {this.state.vehicles.length === 0
             ? 'Your garage is currently empty'
             : this.state.vehicles.map(vehicle => (
-              <div key={vehicle.vehicleId}>
-                {vehicle.year} {vehicle.make} {vehicle.model}
-                <a href={'#edit-vehicle?id=' + vehicle.vehicleId}>Edit Vehicle</a>
-                {/* <button>Edit Vehicle</button> */}
-              </div>
+              <Card className="garage-card-container" key={vehicle.vehicleId}>
+                <div className="garage-card-div">
+                  <h4 className="garage-card-title">
+                    <FaArrowAltCircleDown className='arrow-btn'/>
+                    {vehicle.year} {vehicle.make} {vehicle.model}
+                  </h4>
+                  <div className="vehicle-details">
+                    <h6 className="vehicle-info">Vehicle Info:</h6>
+                    <p className="vehicle-info">
+                      License Plate: {vehicle.licensePlate}
+                    </p>
+                    <p className="vehicle-info">
+                      Odometer: {vehicle.odometer}
+                    </p>
+                    <p className="vehicle-info">
+                      Additional Notes: {vehicle.notes}
+                    </p>
+                  </div>
+                  <div className="service-details">
+                    <h6>Service Info:</h6>
+                    <p className="service-info">Milage at time of service:</p>
+                    <p className="service-info">Service Performed:</p>
+                  </div>
+                  <div className="garage-card-btns">
+                    <a
+                        className="add-service-btn"
+                        href={`#edit-vehicle?id=${vehicle.vehicleId}`}>
+                      Add Service
+                    </a>
+                    <a
+                        className="edit-vehicle-btn"
+                        href={`#edit-vehicle?id=${vehicle.vehicleId}`}>
+                      Edit Vehicle
+                    </a>
+                    <a
+                        className="delete-vehicle-btn"
+                        href={`#edit-vehicle?id=${vehicle.vehicleId}`}>
+                      Delete Vehicle
+                    </a>
+                  </div>
+                </div>
+              </Card>
             ))}
         </div>
       </div>
--- server/index.js
+++ server/index.js
@@ -47,9 +47,9 @@ app.get('/api/vehicleData', (req, res, next) => {
 app.post('/api/vehicleData', (req, res, next) => {
   const { year, make, model, licensePlate, odometer, notes } = req.body;
   if (!year || !make || !model || !licensePlate || !odometer || !notes) {
-    throw new ClientError(400, 'year, make, model, license plate, odometer, and notes are required fields');
+    throw new ClientError(400, 'Error: Year, make, model, license plate, odometer, and notes are required fields');
   } else if (isNaN(year) || isNaN(odometer)) {
-    throw new ClientError(400, 'year and odometer must be a number.');
+    throw new ClientError(400, 'Error: Year and odometer must be a number, with no commas.');
   }
   const sql = `
     insert into "vehicle" ("year", "make", "model", "licensePlate", "odometer", "notes")
