3ecc8fd07066537c225dbeb554e5e6b48c4bb43b
Aaron Butnik <aaronjbutnik@gmail.com>
2023-01-06 20:32:34 +0000

    Updated within index.js.  Added 'order by' sql code to GET routes so that now vehilces and services will be rendered in the order that they are entered from first vehicle/service entered to last entered. #4

--- server/index.js
+++ server/index.js
@@ -34,6 +34,7 @@ app.get('/api/vehicleData', (req, res, next) => {
            "odometer",
            "notes"
     from "vehicle"
+    order by "vehicleId"
     `;
   db.query(sql)
     .then(result => {
@@ -59,6 +60,7 @@ app.get('/api/serviceData', (req, res, next) => {
            "model"
     from "service"
     join "vehicle" using ("vehicleId")
+    order by "serviceId"
     `;
   db.query(sql)
     .then(result => {
